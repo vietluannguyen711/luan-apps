bf4f37103f13bddc6b12241cca11bc3c271af579
vietluannguyen711 <vietluannguyen711@gmail.com>
2025-08-08 12:46:27 +0700

    web-app-thymeleaf

---
+++ apps/web-app-thymeleaf/src/main/java/com/example/webappthymeleaf/WebAppThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppThymeleafApplication.class, args);
+	}
+
+}
---
+++ apps/web-app-thymeleaf/src/test/java/com/example/webappthymeleaf/WebAppThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
